51d329494ee6b397b2bcfdcd0c440fc5cc756848
VolodymyrKalynii <v.kalynii@gmail.com>
2016-03-27 22:05:56 +0300

    Проект 2

---
+++ pagePsd2/js/JavaScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('.nav').hover(function () {
+        $('.nd-on720').css('visibility', 'visible');
+        $('.nav').css('height', '230px');
+    }, function () {
+        $('.nd-on720').css('visibility', 'hidden');
+        $('.nav').css('height', '30px');
+    })
+})
+
+function windowSize() {
+    if ($(window).width() < '721') {
+        $('.nd-on720').css('visibility', 'hidden');
+    } else {
+        $('.nd-on720').css('visibility', 'visible');
+    }
+}
+$(window).on('load resize', windowSize);
