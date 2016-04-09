779288c0941bffee6a8ebfc447c24fcedd800c53
VolodymyrKalynii <v.kalynii@gmail.com>
2016-04-09 13:07:52 +0300

    Завантажив третій проект

---
+++ pagePsd3/script/JavaScript.js
@@ -0,0 +1,6 @@
+﻿//function fontSize() {
+//    if ($('html').width() <= 960)
+//        $('.html').css({ fontSize: 19 + 'px' });
+//}
+//$(function () { fontSize(); });
+//$(window).resize(function () { fontSize(); });
